de06e8d401a4685189f78eeb8ff5aa687511c27e
abc <def>
2021-12-02 16:24:56 -0800

    first commit

---
+++ src/main/java/BinarySearch.java
@@ -0,0 +1,5 @@
+class BinarySearch {
+    public static int search(int[] array, int target) {
+        return -1;  // place holder
+    }
+}
---
+++ src/test/java/BinarySearchTest.java
@@ -0,0 +1,7 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class BinarySearchTest {
+}
